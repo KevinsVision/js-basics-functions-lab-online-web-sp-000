c21168217847cc58c44e9aefa5feb1ca794bb396
Kevin Lopez Cifuentes <US.Kevin@icloud.com>
2019-09-06 16:42:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+  let result
+    if (someValue === 43) {
+      result = 1
+    }
+    else if (someValue === 50){
+      result = 8
+    }
+    else {
+      result = 8
+    }
+    return result
+}
+
+function distanceFromHqInFeet(someValue) {
+  let result
+  // distanceFromHqInBlocks(someValue)
+    if (someValue === 43) {
+      result = 264
+    }
+    else if (someValue === 50) {
+      result = 2112
+    }
+    else  {
+      result = 2112
+    }
+  return result
+ }
+
+function distanceTravelledInFeet(north, south) {
+  let result
+  if (north < south) {
+    result = (south - north) * 264
+  }
+  else {
+    result = (north - south) * 264
+  }
+  return result
+}
+
+function calculatesFarePrice(north, south) {
+
+  let feet = distanceTravelledInFeet(north, south)
+
+  if (feet <= 400) {
+    return 0
+  }
+  else if (feet > 400 && feet <= 2000) {
+    return  .02 * (feet - 400)
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return  25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
